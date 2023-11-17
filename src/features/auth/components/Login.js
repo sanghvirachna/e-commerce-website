c1f75ced5b2410dfed9eb295e4dc8247610165b0
sanghvirachna <rachnasanghvi123@gmail.com>
2023-11-17 20:56:54 +0530

    webhooks

--- src/features/auth/components/Login.js
+++ src/features/auth/components/Login.js
@@ -60,9 +60,12 @@ export default function Login() {
                   type="email"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
-                {errors.email && (
+                 {errors.email && (
                   <p className="text-red-500">{errors.email.message}</p>
-                )}
+                  // <p className="text-red-500">Email wrong</p>
+                )} 
+               
+
               </div>
             </div>
 
@@ -94,9 +97,13 @@ export default function Login() {
                 />
                 {errors.password && (
                   <p className="text-red-500">{errors.password.message}</p>
+                  //  <p className="text-red-500">Password is not correct</p>
+                  
+
                 )}
               </div>
               {error && <p className="text-red-500">{error || error.message}</p>}
+              {/* {error && <p className="text-red-500">authenticate</p>} */}
             </div>
 
             <div>
